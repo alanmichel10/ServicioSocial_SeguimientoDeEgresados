e84c19a1d174d1ed20aaf507410bc4be7ec722bf
Cristian <cristiangonzalez180401@gmail.com>
2024-09-25 09:48:24 -0600

    Unificacion del nuevo login

--- src/app.py
+++ src/app.py
@@ -40,7 +40,7 @@ def role_required(required_role):
 
 @app.route('/')
 def index():
-    return redirect(url_for('login'))
+    return redirect(url_for('index.html'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
